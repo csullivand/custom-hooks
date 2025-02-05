f6e49a6168edc94b31c9898c1de2cb8bd7acb4dd
christopher sullivan <suhu210712@gmail.com>
2025-02-05 14:03:49 -0600

    primer commit\

---
+++ useTodos.js
@@ -0,0 +1,48 @@
+import { useEffect, useReducer } from "react";
+import { todoReducer } from "../08-useReducer/todoReducer";
+
+const init = ()=>{
+    return JSON.parse(localStorage.getItem("todos")) || [];
+}
+export const useTodos = () => {
+    const [todos, dispatch] = useReducer(todoReducer, [],init);
+
+    useEffect(() => {
+      localStorage.setItem("todos", JSON.stringify(todos));
+    
+    }, [todos]);
+    
+    const handleNewTodo = (newTodo) => {
+        const action = {
+            type: 'Add todo',
+            payload: newTodo
+        }
+
+        dispatch(action);
+    }
+
+    const handleDeleteTodo = (id)=>{
+        dispatch({
+            type: 'Delete todo',
+            payload: id
+        })
+
+    }
+
+    const handleToggleTodo = (id)=>{
+
+
+        dispatch({
+            type: 'Toggle todo',
+            payload: id
+        })
+    }
+    return {
+        todos,
+        todosCount: todos.length, 
+        pendingTodos : todos.filter(todo => !todo.done).length,
+        handleNewTodo,
+        handleDeleteTodo,
+        handleToggleTodo
+    }
+}
